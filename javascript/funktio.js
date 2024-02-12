b13cedfee481d149cbb5633d4e52ac1750f5343a
MioPortimo <143484280+MioPortimo@users.noreply.github.com>
2024-02-12 18:48:09 +0200

    Update funktio.js

--- javascript/funktio.js
+++ javascript/funktio.js
@@ -1,26 +1,15 @@
-const getRandomIntNumberInRange = (min,max)=>{
-    return Math.floor(Math.random()*max) + min;
-}
-document.querySelector("#dice").addEventListener("click",()=>{
-    const randomized_number = getRandomIntNumberInRange(1,6)
-    const diceImage = document.querySelector("#dice");
+document.addEventListener('DOMContentLoaded', function () {
+    var diceElement = document.getElementById('dice');
+    var diceImageElement = document.getElementById('diceImage');
 
-    if (randomized_number==1){
-        diceImage.src=".//kuvat/dice_1.png";
-    }
-    else if(randomized_number==2){
-        diceImage.src=".//kuvat/dice_2.png";
-    }
-    else if(randomized_number==3){
-        diceImage.src=".//kuvat/dice_3.png";
-    }
-    else if(randomized_number==4){
-        diceImage.src=".//kuvat/dice_4.png";
-    }
-    else if(randomized_number==5){
-        diceImage.src=".//kuvat/dice_5.png";
-    }
-    else if(randomized_number==6){
-        diceImage.src=".//kuvat/dice_6.png";
-    }
-})
+    diceElement.addEventListener('click', function () {
+        // Arvotaan satunnaisluku välillä 1-6
+        var randomNumber = Math.floor(Math.random() * 6) + 1;
+
+        // Päivitetään nopan kuva
+        diceImageElement.src = 'img/' + randomNumber + '.png';
+
+        // Päivitetään näytettävä numero
+        outputElement.textContent = 'Arvottu numero: ' + randomNumber;
+    });
+});
